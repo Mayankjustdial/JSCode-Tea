f82a949a386a42586b3903c63a15be0bf8ac3923
MAYANK KUMAR <67697531+Mayankjustdial@users.noreply.github.com>
2023-12-01 05:10:57 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = "33"
+// let score1 = "33dfdf"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// let valueInNumber1 = Number(score1)
+
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber1);
+
+// "33" =>33
+// "33abd" => NaN
+//  true => 1; false =>0
+
+// let isLoggedIn = "Mayank"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 =>true; 0=>false
+// ""=>false
+// "mayank" => true
+
+let someNumber =33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
