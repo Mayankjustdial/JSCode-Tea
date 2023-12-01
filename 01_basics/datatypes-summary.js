b624da118542e07b90fdc54926fdc9a72d4f13cd
MAYANK KUMAR <67697531+Mayankjustdial@users.noreply.github.com>
2023-12-01 05:56:16 +0000

    Primitive and non-primitive

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -32,4 +32,28 @@ const myFunction = function(){
  console.log("Hello world");
 }
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
+
+
+
+
+
+// *********************************************
+// Stack (Prmitive), Heap (Non-primtive)
+let myYoutubeName = "techbeetalks"
+let anothername = myYoutubeName
+anothername = "mayankyoutube"
+console.log(anothername);
+console.log(myYoutubeName);
+
+
+let userOne = {
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "mynk@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
