ae354e2040dcf133926b1811507eb8304ccd73bf
MAYANK KUMAR <67697531+Mayankjustdial@users.noreply.github.com>
2023-12-02 04:40:15 +0000

    Maths_random

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "mynk"
+const repoCnt = 40
+// console.log(name + repoCnt + " value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCnt}`);
+
+const gameName = new String('mayankkumar')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('3'));
+// console.log(gameName.indexOf('y'));
+
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-6,4)
+// console.log(anotherString);
+
+// const newStringOne = "    Mynk     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://mayank.com/mayank%20kumar"
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('mayank'))
+// console.log(url.includes('mynk'))
+
+
+console.log(gameName.split('-'));
