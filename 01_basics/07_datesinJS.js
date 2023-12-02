72e7527aae6c5649c456fa3d390f06000e2ad4ba
MAYANK KUMAR <67697531+Mayankjustdial@users.noreply.github.com>
2023-12-02 04:56:00 +0000

    DateinJS

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,24 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let mycreatedDate = new Date(2023,0,23)
+// let mycreatedDate = new Date(2023,0,23,5,3)
+
+// console.log(mycreatedDate.toLocaleString());
+
+let mycreatedDate = new Date("01-14-2023")
+
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(mycreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
