d3a79b3dbfb3b78b2f443a8516d66d5de2501e27
MAYANK KUMAR <67697531+Mayankjustdial@users.noreply.github.com>
2023-12-03 04:32:31 +0000

    arrow & this keyword

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,54 @@
+const user = {
+    username: "mayank",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "mayank"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "mayank"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "mayank"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "mayank"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
